72778c463a0932b932fb9bcbb1d907781ffd13d0
YenBui <tolifetofight@gmail.com>
2022-04-15 11:45:51 +0700

    Update streamlit_app.py
    pre-populate the list to set an example for the customer.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,6 @@ streamlit.text('🥑 🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(my_fruit_list)
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
 
